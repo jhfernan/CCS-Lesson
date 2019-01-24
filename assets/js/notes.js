6d7eca66ed5e1b3d5886c21f2d3540a456d0ec20
Jonathan Fernandes <jhfernan@mail.usf.edu>
2019-01-24 12:46:31 -0500

    Updated lesson

---
+++ assets/js/notes.js
@@ -0,0 +1,40 @@
+// First bit
+alert("Hello world!");
+
+// Variables
+// The building blocks of programming
+// What are variables?
+// How can we use them (revisit hello world with name variable)
+var name = "Jonathan";
+
+alert("Hello " + name + "!")
+
+// Show interpolation as another way to manipulate a string
+
+// Other types and examples:
+// Boolean - admin example
+// Number - score
+// Array - cover in depth later
+// Object - cover in depth later
+
+// Functions and uses
+Math.floor((Math.random() * 10) + 1);
+
+// Break it down and then put it in a function
+
+// If else statements
+// use greeting as an example based on name
+// cover operators
+
+// While statement with prompt
+let answer = prompt("What is your name?")
+console.log(answer)
+
+
+// Guessing game
+
+// Plan:
+// Make a random number
+
+// create while statement that prompts for number until they give it to you
+// compare answer and break if it is prompt again if not
---
+++ assets/js/notes2.js
@@ -0,0 +1,10 @@
+// shopping list
+// Arrays deep dive
+
+// What can they do?
+// How do you manipulate the data? CRUD
+
+// Objects deep dive
+// Start by talking about procedural vs object oriented programming
+
+// Talk about how objects can make your code cleaner
